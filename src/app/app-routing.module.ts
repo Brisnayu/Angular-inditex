f8c5a14ce14e059c2fde6ba1d6e2b6b9c6813d45
BrisnaPaez <brisnapaez.13@gmail.com>
2024-06-15 20:27:31 +0200

    Added structure form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calculos',
+    loadChildren: () => import('./calculation/calculation.module').then(mod => mod.CalculationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
